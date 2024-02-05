ce81b8cef795b94b63ba4c0fc078a7ab217efff1
bohdan <shovkopliasbohdan@gmail.com>
2024-02-05 23:04:53 +0200

    userName field1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+const product = {
+   id: "12123",
+   author: "oleg",
+   productName: "Oleg",
+   description: "232323",
+   startValue: "123123",
+   contact: ""
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [userName, setUserName] = useState("");
+   const [displayName, setDisplayName] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   const [popapus, setPopapus] = useState(true);
+
+   useEffect(() => {
+      setDisplayName((nm) => {
+         const savedName = localStorage.getItem("name");
+         if (savedName) {
+            setPopapus((p)=>false)
+            return savedName;
+         }
+         return "Guest342";
+      });
+   }, []);
+   return (
+      <>
+         <h1>{displayName}</h1>
+         {popapus && <p>Wake up to reality</p>}
+         <div className="card">
+            <form
+               action=""
+               onSubmit={(e) => {
+                  e.preventDefault();
+                  localStorage.setItem("name", userName);
+                  setDisplayName((nm) => userName);
+               }}
+            >
+               <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="name"
+                  value={userName}
+                  onChange={(e) => {
+                     setUserName((nm) => e.target.value);
+                  }}
+               />
+            </form>
+            <p>
+               Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+         </div>
+         <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+         </p>
+      </>
+   );
 }
 
-export default App
+export default App;
---
+++ src/components/UserName.jsx
@@ -0,0 +1,20 @@
+function UserName() {
+   return (
+      <div>
+         <p></p>
+      </div>
+   );
+}
+
+function editForm() {
+   return (
+      <>
+         <form action="">
+            <input type="text"/>
+         </form>
+         
+      </>
+   );
+}
+
+export default UserName;
